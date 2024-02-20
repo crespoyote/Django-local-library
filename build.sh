ed56e87baacb377dc50419dd76fffadfd958f25e
Alfonso Crespo <alfonsocrespobelda@gmail.com>
2024-02-20 15:31:32 +0100

    mensaje

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ python3 manage.py makemigrations
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 
-python3 populate_catalog.py
+#python3 populate_catalog.py
 
 python manage.py createsuperuser
